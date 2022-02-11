c9631dcb8712d48c549455f43c67da02dc91e856
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-02-11 23:52:37 +0530

    adding First name validation

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -10,7 +10,12 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello welcome to user Registration");
+            Console.WriteLine("Welcome User  for Registration");
+            Console.WriteLine("*First letter should be capital.");
+            Console.WriteLine("*length should be Minimum three characters.");
+            Console.WriteLine("Enter First name :");
+            string firstName = Console.ReadLine();     
+            REGEXPattern.CheckFirstName(firstName);
             Console.ReadLine();
         }
     }
---
+++ UserRegistration/UserRegistration/REGEXPattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    internal class REGEXPattern
+    {
+        public  static void CheckFirstName(string name)
+        {
+            string firstNamePattern = "^[A-Z][a-z]{3,}$";
+            if (Regex.IsMatch(name, firstNamePattern))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+                Console.WriteLine("Invalid name!!");
+        }
+
+    }
+}
